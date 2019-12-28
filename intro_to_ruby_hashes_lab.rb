598d33dd2f1471da45abfceccf1a7f571e7880ce
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 04:43:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
-        :shortline => {}
+        :shortline_railroad => {}
       }
     }
   }
@@ -68,7 +68,7 @@ def monopoly_with_fourth_tier
           :b_and_o_railroad => {
             "mortgage_value" => "$400"
           },
-          :shortline => {
+          :shortline_railroad => {
             "mortgage_value" => "$800"
         }
       }
